707054dad4087c63a03ea64e4a6ab64d5453ea01
LucaFeltrin14 <lucasf6@al.insper.edu.br>
2024-10-16 19:41:42 -0300

    style: Estilo do Header e logo alterados
    tste
    
    teste

--- home-page/src/components/Header.js
+++ home-page/src/components/Header.js
@@ -1,22 +1,53 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import logo from '../assets/images/ic.jpg'; 
 import '../styles/Header.css';
 
 const Header = () => {
-    return (
-      <header className="header">
-        <img src={logo} alt="Logo" className="logo" />
-        <nav className="nav">
-          <ul className="nav-list">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Serviços</a></li>
-            <li><a href="#">Projetos</a></li>
-            <li><a href="#">Sobre</a></li>
-            <li><a href="#">Contatos</a></li>
-          </ul>
-        </nav>
-      </header>
-    );
+  const [activeLink, setActiveLink] = useState('Home');
+  const headerRef = useRef(null);
+  const gradientRef = useRef(null);
+
+  useEffect(() => {
+    const header = headerRef.current;
+    const gradient = gradientRef.current;
+
+    const handleMouseMove = (e) => {
+      const { clientX, clientY } = e;
+      const { offsetLeft, offsetTop } = header;
+      const x = clientX - offsetLeft;
+      const y = clientY - offsetTop;
+      gradient.style.setProperty('--x', `${x}px`);
+      gradient.style.setProperty('--y', `${y}px`);
+    };
+
+    header.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      header.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  const handleNavClick = (link) => {
+    setActiveLink(link);
+  };
+
+  return (
+    <header className="header" ref={headerRef}>
+      <img src={logo} alt="InsperCoding" className="logo" />
+      <nav className="nav">
+        <ul className="nav-list">
+          <li><a href="#" className={activeLink === 'Home' ? 'active' : ''} onClick={() => handleNavClick('Home')}>Home</a></li>
+          <li><a href="#" className={activeLink === 'Serviços' ? 'active' : ''} onClick={() => handleNavClick('Serviços')}>Serviços</a></li>
+          <li><a href="#" className={activeLink === 'Projetos' ? 'active' : ''} onClick={() => handleNavClick('Projetos')}>Projetos</a></li>
+          <li><a href="#" className={activeLink === 'Sobre' ? 'active' : ''} onClick={() => handleNavClick('Sobre')}>Sobre</a></li>
+          <li><a href="#" className={activeLink === 'Contato' ? 'active' : ''} onClick={() => handleNavClick('Contato')}>Contato</a></li>
+        </ul>
+      </nav>
+      <div className="header-hover-effect">
+        <div className="header-hover-effect-gradient" ref={gradientRef}></div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
